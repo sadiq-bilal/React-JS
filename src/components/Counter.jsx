6651233c5576861108cb1b841ad2d2ae8bcb8eef
sadiq-bilal <sadiqbilal2018@gmail.com>
2026-08-24 21:28:14 +0530

    Initial commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useState } from 'react';
+
+function Counter() {
+    const [count, setCount] = useState(0);
+    const [prevCount, setprevCount] = useState(0);
+
+    const increment = () => {
+        setprevCount(count)
+        setCount(count + 1);
+    } 
+    const decrement = () => {
+        setprevCount(count)
+        setCount(count - 1);
+    } 
+    const reset = () => {
+        setprevCount(count)
+        setCount(0);
+    }
+    
+  return (
+    <div>
+          <h1>React Counter App</h1>
+          <div className="container">{count}</div>
+          <button id='btn1' onClick={decrement}>-</button>
+          <button id='btn2' onClick={reset}>Reset</button>
+          <button id='btn3' onClick={increment}>+</button>
+          <p >Counter changed from {prevCount} to {count}</p>
+    </div>
+  )
+}
+
+export default Counter;
